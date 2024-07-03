ab153ee5951c15e5a78e98e6a49c74e3398d888e
NayamulNirob <158471735+NayamulNirob@users.noreply.github.com>
2024-07-03 22:20:42 +0600

    Add

--- FirstJspProject/src/java/utill/Dbutill.java
+++ FirstJspProject/src/java/utill/Dbutill.java
@@ -9,7 +9,7 @@ import java.util.logging.Logger;
 
 public class Dbutill {
     static Connection con=null;
-    static String url="jdbc:mysql://localhost:3306/dbcrued";
+    static String url="jdbc:mysql://localhost:3306/dbcrud";
     static String user="root";
     static String password="2020";
     static String driver="com.mysql.cj.jdbc.Driver";
